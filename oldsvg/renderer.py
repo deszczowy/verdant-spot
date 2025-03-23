3cf9587a48d27c82269fb33534d24112849b67c4
deszczowy <krystian.szklarek@op.pl>
2025-03-23 22:18:00 +0100

    Old svg classes removed

--- oldsvg/renderer.py
+++
@@ -1,9 +0,0 @@
-from .document import SvgDocument
-
-class ProjectRenderer:
-    
-    def render(self, document: SvgDocument) -> str:
-        content = ""
-        for layer in document.layers:
-            content += layer.render()
-        return document.render(content)
